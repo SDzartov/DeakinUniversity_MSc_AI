db8ee6f12cd72427a348b4e2ab6f0a3c8a3e25b5
ShaneD <shane.dzartov@bigpond.com>
2025-04-09 12:47:41 +1000

    commit

---
+++ BotFormRecognizer/Settings.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotFormRecognizer
+{
+    internal static class Settings
+    {
+        public static string ConnectionString = "";
+        public static string ContainerName = "formbotstorage";
+        public static string Key = "";
+        public static string Endpoint = "https://deakin19073460464.blob.core.windows.net/formbotstorage";
+
+        public static string FormKey = "";
+        public static string FormEndpoint = "https://formreconshane01.cognitiveservices.azure.com/";
+        
+    }
+}
